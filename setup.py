6943fa094aa8aaa4484713f4acf2e231c3d3686a
Jeffrey Leary <jeff@Jeffreys-MacBook-Air.local>
2016-12-12 10:47:27 -0500

    license and config updates

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from distutils.core import setup
 setup(
   name = 'remrunner',
-  packages = ['remrunner'], # this must be the same as the name above
+  packages = ['remrunner'], 
   version = '0.1',
   description = 'Transfer a local script file to a remote host and execute it.',
   author = 'Jeff Leary',
   author_email = 'sillymonkeysoftware@gmail.com',
-  url = 'https://github.com/jeffleary00/remrunner', # use the URL to the github repo
-  download_url = 'https://github.com/jeffleary00/remrunner/tarball/0.1', # I'll explain this in a second
-  keywords = ['automation', 'ssh', 'command', 'paramiko'], # arbitrary keywords
-  classifiers = [],
+  url = 'https://github.com/jeffleary00/remrunner',
+  download_url = 'https://github.com/jeffleary00/remrunner/tarball/0.1',
+  install_requires=['paramiko'],
+  keywords = ['automation', 'ssh', 'command', 'paramiko'],
+  classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+  ],
 )
